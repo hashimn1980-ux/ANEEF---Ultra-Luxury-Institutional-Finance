e47e1bd232c2048bc81940551fa869d15dde4f32
hashimn1980-ux <hashimn1980@gmail.com>
2026-01-05 14:09:55 +0400

    refactor: Remove unused useEffect in App
    The useEffect hook was imported but not used within the App component. Removing it cleans up the code and reduces unnecessary imports.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Loader from './components/Loader';
